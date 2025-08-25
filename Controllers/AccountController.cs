e8c6016694b8f9d3215ba26e5736ce094c649f83
Jonatas Silva <jonsilvasantos7@gmail.com>
2025-08-25 11:08:06 -0300

    Update AccountController.cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -86,49 +86,7 @@ public class AccountController : ControllerBase
             return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
         }
     }
-
-    /*[Authorize]
-       [HttpPost("v1/accounts/upload-image")]
-       public async Task<IActionResult> UploadImage(
-           [FromBody] UploadImageViewModel model,
-           [FromServices] BlogDataContext context)
-       {
-           var fileName = $"{Guid.NewGuid().ToString()}.jpg";
-           var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
-           var bytes = Convert.FromBase64String(data);
-
-           try
-           {
-               await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
-           }
-           catch (Exception ex)
-           {
-               return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
-           }
-
-           var user = await context
-               .Users
-               .FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
-
-           if (user == null)
-               return NotFound(new ResultViewModel<Category>("Usuário não encontrado"));
-
-           user.Image = $"https://localhost:0000/images/{fileName}";
-           try
-           {
-               context.Users.Update(user);
-               await context.SaveChangesAsync();
-           }
-           catch (Exception ex)
-           {
-               return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
-           }
-
-           return Ok(new ResultViewModel<string>("Imagem alterada com sucesso!", null));
-       }*/
-
-
-
+    
     [Authorize]
     [HttpPost("v1/accounts/upload-image")]
     public async Task<IActionResult> UploadImage(
